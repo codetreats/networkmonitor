98d794ccc8d6fc7749d70ff6c6a79505fa3cfc54
Martin <github@msturm.net>
2023-08-04 16:43:53 +0200

    add execution debounce

--- container/start.sh
+++ container/start.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
+WAIT_DURATION=1800
 
 chmod +x /*.sh
 ./compile.sh
 service apache2 start
 
+LAST_EXECUTION=0
 while true; do
-  /monitor.sh
-  sleep 30  
+    sleep 30
+    CURRENT_TIME=$(date +%s)
+    DIFF=$((CURRENT_TIME - LAST_EXECUTION))
+    if [[ $DIFF -ge $WAIT_DURATION ]]; then
+        /monitor.sh
+        LAST_EXECUTION=$CURRENT_TIME
+    fi
+    CHANGED=$(find /monitor/data -mtime -0.005)
+    if [[ $CHANGED -gt 0 ]]; then
+        /monitor.sh
+        LAST_EXECUTION=$CURRENT_TIME
+    fi
 done
