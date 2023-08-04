97f4d1d2e4e72264b3239c560dcdb8b7e3abe8fe
Martin <github@msturm.net>
2023-08-04 17:03:56 +0200

    add debug info

--- container/start.sh
+++ container/start.sh
@@ -10,13 +10,16 @@ while true; do
     CURRENT_TIME=$(date +%s)
     DIFF=$((CURRENT_TIME - LAST_EXECUTION))
     if [[ $DIFF -ge $WAIT_DURATION ]]; then
+        echo "$(date) TIMER" >> /monitor/logs/run.log
         /monitor.sh
         LAST_EXECUTION=$CURRENT_TIME
     fi
     CHANGED=$(find /monitor/data -mtime -0.005 | wc -l)
     if [[ $CHANGED -gt 0 ]]; then
+        echo "$(date) MODIFIED" >> /monitor/logs/run.log
         /monitor.sh
         LAST_EXECUTION=$CURRENT_TIME
     fi
+    echo "$(date) SLEEP - DIFF: $DIFF, CHANGED: $CHANGED" >> /monitor/logs/run.log
     sleep 30
 done
