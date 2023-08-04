94cc7584d2ea8f7c29199575343518b7b8eefb37
Martin <github@msturm.net>
2023-08-04 16:49:26 +0200

    minor fix

--- container/start.sh
+++ container/start.sh
@@ -6,17 +6,17 @@ chmod +x /*.sh
 service apache2 start
 
 LAST_EXECUTION=0
-while true; do
-    sleep 30
+while true; do    
     CURRENT_TIME=$(date +%s)
     DIFF=$((CURRENT_TIME - LAST_EXECUTION))
     if [[ $DIFF -ge $WAIT_DURATION ]]; then
         /monitor.sh
         LAST_EXECUTION=$CURRENT_TIME
     fi
-    CHANGED=$(find /monitor/data -mtime -0.005)
+    CHANGED=$(find /monitor/data -mtime -0.005 | wc -l)
     if [[ $CHANGED -gt 0 ]]; then
         /monitor.sh
         LAST_EXECUTION=$CURRENT_TIME
     fi
+    sleep 30
 done
