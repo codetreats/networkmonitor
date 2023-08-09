5166d580e71ffae0de581cdb665b090fe4dcf6dc
Martin <github@msturm.net>
2023-08-09 10:50:48 +0200

    fix timezone in update

--- container/www/update/inc/monitor.inc.php
+++ container/www/update/inc/monitor.inc.php
@@ -2,7 +2,8 @@
 
 function update($src,$val,$level) {
   $rootdir = "/monitor/data";
-  $time = date('Y-m-d H:i:s');
+  $date = new DateTime();
+  $time = $date->format('Y-m-d H:i:s');
   $src_regex = '/^[A-Za-z0-9][-_A-Za-z0-9]*[A-Za-z0-9](\.[A-Za-z0-9][-_A-Za-z0-9]*[A-Za-z0-9]+)*$/';
   if (preg_match($src_regex, $src)) {
     $file = $rootdir . "/". strtolower($src) . ".txt";
